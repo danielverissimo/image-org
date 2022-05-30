cc33b4212a03a44216a3c4b93f78f71a0413669d
Daniel Eduardo <danyelsanches@gmail.com>
2022-05-29 21:45:33 -0300

    fix

--- image-organizer.py
+++ image-organizer.py
@@ -25,7 +25,7 @@ def debug(msg, show=False):
 debug(f'\n\n--------------------------------------\nRUN {run_identity}\n')
 
 def getInputPaths(path):
-    file_path = path
+    file_path = BASE_INPUT_DIR . path
     if not os.path.isfile(file_path):
         absolute_input_dir = os.path.realpath(file_path)
         input_relatives = os.listdir(file_path)
